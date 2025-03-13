ba108319ad5598f3510e9210314bee5bd328b080
Anypeny <rongard88@gmail.com>
2025-03-13 12:26:50 +0700

    Update Create Errors

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"sort"
@@ -10,7 +11,19 @@ import (
 )
 
 func main() {
-	operation, arr := input()
+
+	var operation string
+	var arr []int
+	var err error
+	for {
+		operation, arr, err = input()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		break
+	}
+
 	switch operation {
 	case "AVG":
 		fmt.Println(AVG(arr))
@@ -54,19 +67,33 @@ func MED(arr []int) float64 {
 }
 
 // обработка ввода
-func input() (string, []int) {
+func input() (string, []int, error) {
 
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println("Введите операцию SUM/MED/AVG")
 	scanner.Scan()
 	operation := scanner.Text()
+	UpOperation := strings.ToUpper(operation)
+	if UpOperation == "SUM" || UpOperation == "MED" || UpOperation == "AVG" {
+
+	} else {
+		return "", nil, errors.New("неверный ввод, введите SUM/MED/AVG")
+	}
 	fmt.Println("Введител числа через запятую")
 	scanner.Scan()
 	input := scanner.Text()
-	values := strings.Split(input, ",")
-	arr := make([]int, len(values))
-	for i := range values {
-		arr[i], _ = strconv.Atoi(values[i])
+	if len(input) == 0 {
+		return "", nil, errors.New("ошибка, введите числа через запятую")
 	}
-	return operation, arr
+	values := strings.Split(input, ",")
+
+	var arr []int
+	for i, _ := range values {
+		n, err := strconv.Atoi(values[i])
+		if err != nil {
+			return "", nil, errors.New("ошибка при вводе, вводить числа через запятую только")
+		}
+		arr = append(arr, n)
+	}
+	return UpOperation, arr, nil
 }
