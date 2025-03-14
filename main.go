f9dc2c1fc28f82aba20da010aba82bcbb0483bb0
Anypeny <rongard88@gmail.com>
2025-03-14 18:56:23 +0700

    Create new func

--- main.go
+++ main.go
@@ -1,108 +1,104 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
-//ошибки
-
 const USDtoEUR float64 = 0.92  //стоимость доллара к евро
 const USDtoRUB float64 = 89.10 //стоимость доллара к рублю
+
 func main() {
+	for {
+		fromCur := getCurInput("Введите исходную валюту (USD, RUB, EUR): ")
 
-	//EURtoRUB := USDtoRUB / USDtoEUR
-	//fmt.Printf("%.2f", EURtoRUB)
-	x1, x2, x3 := input() // строка число строка
+		amount := getAmount("Введите количество валюты: ")
 
-	//fmt.Println(x1, x2, x3)
-	fmt.Printf("%.2f", curConv(x1, x2, x3))
+		toCur := getCurInput(fmt.Sprintf("Введите валюту конвертации (кроме %s): ", fromCur))
+
+		res, err := curConv(fromCur, amount, toCur)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		fmt.Printf("%.2f", res)
+		break
+	}
 
 }
 
-// Считываем данные пользователя
-func input() (string, int, string) {
-	var cur1, cur2 string
+// Считываем  валюту
+func getCurInput(s string) string {
+	var cur1 string
+	for {
+		fmt.Println(s)
+		fmt.Scan(&cur1)
+
+		if cur1 != "USD" && cur1 != "EUR" && cur1 != "RUB" {
+			fmt.Println("Неверная валюта")
+			continue
+		}
+		break
+	}
+	return cur1
+}
+
+// Считываем количество валюты
+func getAmount(s string) int {
 	var amount int
 	for {
-		fmt.Println("Введите исходную валюту USD/RUB/EUR")
-		fmt.Scan(&cur1)
-		err := ValidCur(cur1)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		break
-	}
-
-	for {
-		fmt.Println("Введите количество валюты")
+		fmt.Println(s)
 		fmt.Scan(&amount)
-		err := ValidCuant(amount)
-		if err != nil {
-			fmt.Println(err)
+		if amount <= 0 {
+			fmt.Println("неверное количество")
 			continue
-		}
 
-		break
-	}
-
-	switch {
-	case cur1 == "USD":
-		fmt.Println("Введите валюту конвертации RUB/EUR")
-	case cur1 == "EUR":
-		fmt.Println("Введите валюту конвертации RUB/USD")
-	case cur1 == "RUB":
-		fmt.Println("Введите валюту конвертации USD/EUR")
-	}
-	for {
-		fmt.Scan(&cur2)
-		err := ValidCur(cur2)
-		if err != nil || cur2 == cur1 {
-			fmt.Println(err)
-			continue
 		}
 		break
 	}
-
-	return cur1, amount, cur2
-
+	return amount
 }
 
-// читаем ошибки валюты
-func ValidCur(a string) error {
-	if a != "USD" && a != "EUR" && a != "RUB" {
-		return errors.New("Некоретный ввод валюты, попробуйте еще раз")
+// конвертируем в USD
+func convertToUSD(currency string, amount int) (float64, error) {
+	switch currency {
+	case "USD":
+		return float64(amount), nil
+	case "EUR":
+		return float64(amount) / USDtoEUR, nil
+	case "RUB":
+		return float64(amount) / USDtoRUB, nil
+	default:
+		return 0, fmt.Errorf("валюта %s не поддерживается1", currency)
 	}
-	return nil
 }
 
-// ошибки ввода количества
-func ValidCuant(a int) error {
-	if a <= 0 {
-		return errors.New("Неверные данный1")
+// конвертируем из USD
+func convertFromUSD(currency string, usdAmount float64) (float64, error) {
+	switch currency {
+	case "USD":
+		return usdAmount, nil
+	case "EUR":
+		return usdAmount * USDtoEUR, nil
+	case "RUB":
+		return usdAmount * USDtoRUB, nil
+	default:
+		return 0, fmt.Errorf("валюта %s не поддерживается2", currency)
 	}
-	return nil
 }
 
-// расчет валюты
-func curConv(cur1 string, a int, cur2 string) float64 {
-	//var res  float64
-	switch {
-	case cur1 == "USD" && cur2 == "RUB":
-		return USDtoRUB * float64(a)
-	case cur1 == "USD" && cur2 == "EUR":
-		return USDtoEUR * float64(a)
-	case cur1 == "RUB" && cur2 == "USD":
-		return float64(a) / USDtoRUB
-	case cur1 == "RUB" && cur2 == "EUR":
-		return float64(a) / USDtoRUB * USDtoEUR
-	case cur1 == "EUR" && cur2 == "RUB":
-		return USDtoRUB / USDtoEUR * float64(a)
-	case cur1 == "EUR" && cur2 == "USD":
-		return 1 / USDtoEUR * float64(a)
-
+// конвертация через USD
+func curConv(fromCur string, amount int, toCur string) (float64, error) {
+	// Конвертируем в USD
+	usdAmount, err := convertToUSD(fromCur, amount)
+	if err != nil {
+		return 0, err
 	}
-	return 0
+	// Конвертируем из USD
+	result, err := convertFromUSD(toCur, usdAmount)
+	if err != nil {
+		return 0, err
+	}
 
+	return result, nil
 }
