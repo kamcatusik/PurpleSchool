60c7bbc5a04b24c0e7537c0b1cfc147f08acff57
Anypeny <rongard88@gmail.com>
2025-03-17 16:04:53 +0700

    Update with pointer

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ type curMap = map[string]map[string]float64
 
 func main() {
 
-	curency := map[string]map[string]float64{
+	curency := curMap{
 		"USD": {
 			"EUR": 0.92,
 			"RUB": 89.10,
@@ -25,19 +25,19 @@ func main() {
 	}
 
 	for {
-		fromCur := getCurInput("Введите исходную валюту (USD, RUB, EUR): ")
+		fromCur := getCurInput("Введите исходную валюту (USD, RUB, EUR): ") //исходная валюта
 
 		amount := getAmount("Введите количество валюты: ")
 
-		toCur := getCurInput(fmt.Sprintf("Введите валюту конвертации (кроме %s): ", fromCur))
+		toCur := getCurInput(fmt.Sprintf("Введите валюту конвертации (кроме %s): ", fromCur)) //конвертируемая валюта
 
-		res, err := curConv(fromCur, amount, toCur, curency)
+		res, err := curConv(fromCur, amount, toCur, &curency) // Конвертируем
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
 
-		fmt.Printf("%.2f", res)
+		fmt.Printf("%s: %.2f", toCur, res)
 		break
 	}
 
@@ -63,22 +63,26 @@ func getCurInput(s string) string {
 // Считываем количество валюты
 func getAmount(s string) int {
 	var amount int
-	for {
-		fmt.Println(s)
-		fmt.Scan(&amount)
-		if amount <= 0 {
-			fmt.Println("неверное количество")
-			continue
 
+	for {
+
+		fmt.Println(s)
+		_, err := fmt.Scan(&amount)
+
+		if err != nil && amount <= 0 {
+			fmt.Println("Введите целое число")
+			fmt.Scanln()
+			continue
 		}
 		break
 	}
 	return amount
+
 }
 
 // конвертация через мап
-func curConv(fromCur string, amount int, toCur string, curency curMap) (float64, error) {
-	insideMap, isBe := curency[fromCur] // Ищем исходную валюту в мапе, если не находим шлем на ***
+func curConv(fromCur string, amount int, toCur string, curency *curMap) (float64, error) {
+	insideMap, isBe := (*curency)[fromCur] // Ищем исходную валюту в мапе, если не находим шлем на ***
 	if !isBe {
 		return 0, fmt.Errorf("Валюта не найдена %s", fromCur)
 
