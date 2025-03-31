1c7e81643fc5dbe1d3bf2a7401fa88acc8a9c7d5
Anypeny <rongard882gmail.com>
2025-03-31 16:43:21 +0700

    Create new branch

--- logger/logger.go
+++ logger/logger.go
@@ -12,8 +12,9 @@ var (
 
 // логирование ошибок
 func LogInit() {
+
 	//открываем файл, записываем в конец файла логи, если нет файла создаем его
-	file, err := os.OpenFile("app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	file, err := os.OpenFile("app.log", os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatalf("Не удалось открыть файл логов %v", err)
 	}
