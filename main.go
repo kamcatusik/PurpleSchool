83abef1817ae2e713a93baaf2c6a64b59dd4146f
Anypeny <rongard88@gmal.com>
2025-04-18 19:58:17 +0700

    Upgrade chanels

--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"sync"
 )
 
-func createSlice(chCreate chan int, wg *sync.WaitGroup) {
-	defer wg.Done()
+func createSlice(chCreate chan int) {
+
 	arr := make([]int, 0)
 	for i := 0; i < 10; i++ {
 		n := rand.Intn(100)
+
 		arr = append(arr, n)
 
 	}
@@ -18,32 +18,27 @@ func createSlice(chCreate chan int, wg *sync.WaitGroup) {
 	for _, elem := range arr {
 		chCreate <- elem
 	}
-
+	close(chCreate)
 }
-func squer(chSquer chan int, chCreate chan int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for i := 0; i < 10; i++ {
-		res := <-chCreate
+func squer(chSquer chan int, chCreate chan int) {
+
+	for res := range chCreate {
 
 		double := res * res
 
 		chSquer <- double
 	}
-
+	close(chSquer)
 }
 
 func main() {
-	var wg sync.WaitGroup
+
 	chCreate := make(chan int)
 	chSquer := make(chan int)
-	wg.Add(2)
-	go createSlice(chCreate, &wg)
-	go squer(chSquer, chCreate, &wg)
-	go func() {
-		wg.Wait()
-		close(chCreate)
-		close(chSquer)
-	}()
+
+	go createSlice(chCreate)
+	go squer(chSquer, chCreate)
+
 	for elem := range chSquer {
 		fmt.Printf("%d ", elem)
 	}
