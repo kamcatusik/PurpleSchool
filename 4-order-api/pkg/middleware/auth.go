4d1791ca03adaf7f6a2b107e1dbf067fafe88b62
Anypeny <rongard88@gmal.com>
2025-05-06 15:20:10 +0700

    Update middleware auth.go (Add Bearer)

--- 4-order-api/pkg/middleware/auth.go
+++ 4-order-api/pkg/middleware/auth.go
@@ -5,6 +5,7 @@ import (
 	jwte "4-order-api/pkg/JWTE"
 	"context"
 	"net/http"
+	"strings"
 )
 
 type key string
@@ -15,13 +16,15 @@ const (
 
 func Auth(next http.HandlerFunc, config *configs.Config) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		token := r.Header.Get("Authorization")
-		if token == "" {
+
+		auth := r.Header.Get("Authorization")
+		if !strings.HasPrefix(auth, "Bearer") {
 			w.WriteHeader(http.StatusUnauthorized)
 			w.Write([]byte(http.StatusText(http.StatusUnauthorized)))
 			return
 		}
 		//парсим токен
+		token := strings.TrimPrefix(auth, "Bearer ")
 		isValid, data := jwte.NewJWT(config.Auth.Secret).Parse(token)
 		if !isValid {
 			w.WriteHeader(http.StatusUnauthorized)
